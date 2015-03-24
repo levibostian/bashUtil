15f533fb53dd8134abfdc8bf226262879c5e2fd3
Levi Bostian <levi.bostian@gmail.com>
2015-03-24 13:37:26 -0500

    Create git history rewrite file to rewrite history.

---
+++ gitHistoryRewrite.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+ 
+# NOTE: This script was *not* made by me. It was 
+# created by GitHub and found here:
+# https://help.github.com/articles/changing-author-info/
+# It is just so great, I wanted to make it more 
+# convenient by adding it to this repo.
+
+git filter-branch --env-filter '
+ 
+OLD_EMAIL="$1"
+CORRECT_NAME="$2"
+CORRECT_EMAIL="$3"
+ 
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
