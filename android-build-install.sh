10570f53d358093ec52235935d52fd866445fb1d
Levi Bostian <levi.bostian@gmail.com>
2018-04-08 15:52:51 -0500

    Update android-build-install.sh to be more complete with notifications.

--- android-build-install.sh
+++ android-build-install.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# Compile and install Android app to device. Launch it after build complete. 
-# $> androidbuildlaunch com.foo.bar NameOfActivity
+# Compile and install Android app to device. Add as many args as you want to add to the g$
+# $> android-build-launch.sh -PminSdk=22 --offline
 
-./gradlew installDevelopmentDebug
-
-# adb shell am start -n $1/$1.$2 # didn't get this to work because my activities are located in package.
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	open -a Android\ Studio
+if ./gradlew installDevelopmentDebug $* ; then 
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		osascript -e 'display notification "Built successfully" with title "Success"'
+		open -a Android\ Studio
+	else 
+		echo 'Success.'
+	fi
+else 
+	if [[ "$OSTYPE" == "darwin"* ]]; then	
+		osascript -e 'display notification "Build failed." with title "FAILED"'
+		open -a Android\ Studio
+	else 
+		echo 'FAILED.'
+	fi
 fi
 
 
